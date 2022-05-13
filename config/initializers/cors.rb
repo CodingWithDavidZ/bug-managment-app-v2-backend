a431c8e5e76f14100823bba1e3554b4e791a107f
David Zombar <davidzombar@gmail.com>
2022-05-13 10:41:42 -0600

    updating CORS again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'tranquil-depths-19820.herokuapp.com'
+    origins 'https://main--steady-travesseiro-d6d661.netlify.app'
 
     resource '*',
              headers: :any,
