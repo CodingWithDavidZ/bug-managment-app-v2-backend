90c658466ef82084a24f2db5913fe2b374984e02
David Zombar <davidzombar@gmail.com>
2022-05-13 10:29:41 -0600

    changed  CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tranquil-depths-19820.herokuapp.com:443'
+    origins 'tranquil-depths-19820.herokuapp.com'
 
     resource '*',
              headers: :any,
