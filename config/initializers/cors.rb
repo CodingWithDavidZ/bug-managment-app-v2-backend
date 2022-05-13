c056596f6587cdee160a2ccd37b7d2c1b9e77bda
David Zombar <davidzombar@gmail.com>
2022-05-13 10:13:29 -0600

    updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'https://main--steady-travesseiro-d6d661.netlify.app/'
 
     resource '*',
              headers: :any,
