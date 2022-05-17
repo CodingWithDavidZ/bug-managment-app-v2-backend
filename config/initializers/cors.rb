f1fd185d4f5b6cd48920f2175dcfa14122017b19
David Zombar <davidzombar@gmail.com>
2022-05-17 16:44:24 -0600

    update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://pure-basin-18390.herokuapp.com/'
+    origins 'https://pure-basin-18390.herokuapp.com'
 
     resource '*',
             headers: :any,
