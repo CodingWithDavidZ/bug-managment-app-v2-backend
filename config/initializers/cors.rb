5e85acc0d5a080de701f12b7057a2406e4117c2e
David Zombar <davidzombar@gmail.com>
2022-05-17 16:39:37 -0600

    deployed frontend on different host, updating Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://steady-travesseiro-d6d661.netlify.app'
+    origins 'https://pure-basin-18390.herokuapp.com/'
 
     resource '*',
             headers: :any,
